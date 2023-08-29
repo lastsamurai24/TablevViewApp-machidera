cdeafcec39d4d3c977de027828d8ab2842d79ac3
samurai <lastsamurai24@icloud.com>
2023-08-29 12:43:01 +0900

    直しました

--- TableViewController.swift
+++ TableViewController.swift
@@ -55,24 +55,39 @@ class TableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        
+        
         if editingStyle == .delete {
             taskArray.remove(at: indexPath.row)
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            UserDefaults.standard.set(taskArray,forKey: "add")
+            tableView.reloadData()
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+   ///     if editingStyle == .delete {
+    //        taskArray.remove(at: indexPath.row)
+            
+    //        tableView.deleteRows(at: [indexPath], with: .fade)
+   //     } else if editingStyle == .insert {
+            
+    //        tableView.reloadData()
+    
+            
+        }
     }
     
 
@@ -101,4 +116,4 @@ class TableViewController: UITableViewController {
     }
     */
 
-}
+
